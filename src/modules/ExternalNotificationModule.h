82c73072db4afe8459b43d37127841e98d5fc229
BilouBrot <github@traut.men>
2025-08-24 19:41:45 +0200

    Change bell timer and offset types to int32_t for better handling of negative values in ExternalNotificationModule

--- src/modules/ExternalNotificationModule.h
+++ src/modules/ExternalNotificationModule.h
@@ -66,7 +66,7 @@ class ExternalNotificationModule : public SinglePortModule, private concurrency:
     bool isMuted = false;
 
     // Custom timer to track when alert bell was received
-    uint32_t alertBellTimerStart = 0;
+    int32_t alertBellTimerStart = 0;
     bool hasReceivedBell = false;
 
     virtual AdminMessageHandleResult handleAdminMessageForModule(const meshtastic_MeshPacket &mp,
